c61cd472d891ed6365ed965f0b38d1aaf0e35973
mr-flake <127868141+mr-flake@users.noreply.github.com>
2023-03-14 15:06:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,7 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
+streamlit.multipselect("Pick some fruits:", list(my_fruit_list.index))
+
+streamlit.dataframe(my_fruit_list)
