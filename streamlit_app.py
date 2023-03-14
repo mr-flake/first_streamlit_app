0a1c283770eff624994bc059c779f3a1076cbfbd
mr-flake <127868141+mr-flake@users.noreply.github.com>
2023-03-14 14:54:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
-streamlit.title('Yo Yo Yo')
-streamlit.text('This is interesting')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
